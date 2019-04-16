f9d1d55ea6c7e1d38c08ad8729ec0bee78b6da8b
Alexey Suleymanov <alex@pstu.ru>
2019-04-16 13:42:19 +0500

    Initial Commit

---
+++ Stocks/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Stocks
+//
+//  Created by Сулейманов Алексей on 16/04/2019.
+//  Copyright © 2019 alex. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
